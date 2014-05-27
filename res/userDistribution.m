c2b499715d27d37501c0ce413ba0f493ecb31df1
Hongjian <shuangzizuobh2@gmail.com>
2014-05-27 11:19:53 +0800

    Matlab analysis on distribution

---
+++ res/userDistribution.m
@@ -0,0 +1,24 @@
+checkinsN = importdata('checkinsN-dist');
+friendsN = importdata('friendsN-dist');
+
+f1 = figure();
+hold on;
+hist(checkinsN, 50);
+title('#Check-ins histrogram');
+
+
+f2 = figure();
+hold on;
+hist(gca, friendsN, 50);
+% set(gca, 'yscale', 'log');
+title('#friends histogram');
+
+
+f3 = figure();
+hold on;
+h1 = cdfplot(checkinsN);
+set(h1, 'linewidth', 3, 'color', 'red');
+h2 = cdfplot(friendsN);
+set(h2, 'linewidth', 3', 'color', 'blue');
+set(gca, 'xscale', 'log');
+legend('#check-ins', '#friends');
